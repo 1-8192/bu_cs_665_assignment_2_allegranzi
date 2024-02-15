7208aea355b5daf846289f936c580e96490a24aa
Alessandro Allegranzi <allegranzia@gmail.com>
2024-02-15 18:32:04 -0500

    finished comments

--- src/main/java/edu/bu/met/cs665/driver/Scooter.java
+++ src/main/java/edu/bu/met/cs665/driver/Scooter.java
@@ -1,7 +1,23 @@
+/**
+ * Name: Alessandro Allegranzi
+ * Course: CS-665 Software Designs & Patterns
+ * Date: 02/22/2024
+ * File Name: Scooter.java
+ * Description: Scooter class that implements Vehicle.
+ */
+
 package edu.bu.met.cs665.driver;
 
+/**
+ * Scooter class that implements vehicle interface.
+ */
 public class Scooter implements Vehicle {
 
+  /**
+   * Class specific implementation of the interface method. Shows the vehicle type.
+   *
+   * @return string the vehicle type.
+   */
   @Override
   public String getVehicleType() {
     return "Scooter";
--- src/main/java/edu/bu/met/cs665/driver/Taxi.java
+++ src/main/java/edu/bu/met/cs665/driver/Taxi.java
@@ -1,9 +1,23 @@
+/**
+ * Name: Alessandro Allegranzi
+ * Course: CS-665 Software Designs & Patterns
+ * Date: 02/22/2024
+ * File Name: Taxi.java
+ * Description: Taxi class that implements Vehicle.
+ */
+
 package edu.bu.met.cs665.driver;
 
-import edu.bu.met.cs665.driver.Vehicle;
-
+/**
+ * Taxi class that implements vehicle interface.
+ */
 public class Taxi implements Vehicle {
 
+  /**
+   * Class specific implementation of the interface method. Shows the vehicle type.
+   *
+   * @return string the vehicle type.
+   */
   @Override
   public String getVehicleType() {
     return "Taxi";
